23b860579a989a6f13da0edf5d9dd42aeda3af8b
Tejas upadhyay <131291931+Tejasupadhyay1508@users.noreply.github.com>
2026-02-28 17:52:46 +0530

    01 setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button} from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex flex-col item-center justify-center h-screen bg-gray-100">
+    <Button>
+      Get started
+    </Button>
+   </div>
+  );
+}
